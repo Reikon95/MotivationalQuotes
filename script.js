32417b3c94bfc4f17efcc39a2f06e7ef2d701e2c
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-06-26 08:50:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ switch(randomNumber) {
     quote = '<h1>Every time I practice, the luckier I get</h1> <p>Gary Player - Golfing legend</p>';
     break;
   case 2:
-    quote = "<h1>Winning isn't everything, but wanting to win is</h1><p>Vince Lombardi</p>";
+    quote = "<h1>Winning isn't everything, but wanting to win is</h1><p>Vince Lombardi - NFL player and coach</p>";
     break;
   case 3:
     quote = "<h1>I've failed over and over again in my life. And that is why I succeed</h1> <p>Michael Jordan - NBA legend</p>";
@@ -22,7 +22,7 @@ switch(randomNumber) {
     quote = '<h1>Champions keep playing till they get it right</h1> <p>Billie Jean King - Tennis legend</p>';
     break;
       case 6:
-    quote = '<h1>You are never a loser until you stop trying</h1> <p>Mike Ditka</p>';
+    quote = '<h1>You are never a loser until you stop trying</h1> <p>Mike Ditka - former NFL player</p>';
     break;
   case 7:
     quote = '<h1>Half the game is mental, the other half is being mental</h1> <p>Jim McKenny - Hockey player</p>';
